8b280409d0ec8afe1ff53bc470bd24d0bac8e7be
Dan Cashion <dangcashion@gmail.com>
2020-02-03 14:47:26 -0600

    working on Evaluation form

--- client/src/components/RentalEvaluationForm.js
+++ client/src/components/RentalEvaluationForm.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
+import './RentalEvaluationForm.css';
 
 const rentalEvaluationForm = () => {
-    return <div>Rental Evaluation Form</div>
+    // const { handleSubmit, register, errors } = useForm();
+    const onSubmit = values => {
+        console.log(values);
+    };
+
+    return <div className='rentalEvaluationContainer'>
+        <h1>Rental Evaluation Form</h1>
+        <div className='rentalInput'>
+            <form>
+                <input name="email" />
+
+                <input name="username" />
+  
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+        <div className='rentalPrediction'>
+            Output
+        </div>
+    </div>
 }
 
 export default rentalEvaluationForm;
