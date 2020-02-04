855fdcfb8e6a92f7daf62c7a62b55a593dd9aae5
Gideon <reachgiddy@gmail.com>
2020-02-04 17:27:50 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-       
+       <h1>Hello</h1>
         <Router exact path="/register">
             <RegisterLogin />
         </Router>
