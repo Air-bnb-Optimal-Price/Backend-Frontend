a7e1a92e7cf59f13517fc8f555ad4d02d479de6f
Gideon <reachgiddy@gmail.com>
2020-02-03 23:03:00 -0500

    Created form for Register section

--- client/src/components/RegisterLoginSection/RegisterContainer/Register.js
+++ client/src/components/RegisterLoginSection/RegisterContainer/Register.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './Register.css';
 
 function Register(props) {
     return (
         <div className="register-container">
-            Register
+            <form>
+                <div className="input-wrapper">
+                    <div className="input-container">
+                        <label htmlFor="user">Username</label>
+                        <input id="user" type='text' placeholder="Enter Username"/>
+                    </div>
+                    <div className="input-container">
+                        <label htmlFor="email">Email</label>
+                        <input id="email" type='email' placeholder="Enter Email"/> 
+                    </div>  
+                    <div className="input-container">
+                        <label htmlFor="password">Password</label>
+                        <input id="password" type='password' placeholder="Enter Password"/> 
+                    </div>
+                    <div className="input-container">
+                        <label htmlFor="comfirm-password">Confirm Password</label>
+                        <input id="comfirm-password" type='password' placeholder="Enter Password again"/> 
+                    </div>
+                
+                    <button type="submit">Register</button> 
+                    <div className="input-container">
+                        <p>Already Signed Up?</p><a href="#">Login</a>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 }
