47a8b7bdc026f87712c584ef9aa480974c5f2ba3
Dan Cashion <dangcashion@gmail.com>
2020-02-05 15:57:54 -0600

    Rental Prediction map

--- client/src/components/RentalEvaluationForm.js
+++ client/src/components/RentalEvaluationForm.js
@@ -25,6 +25,7 @@ const RentalEvaluationForm = () => {
   const [cancellationPolicy, setCancellationPolicy] = useState(0);
   const [bathroomCount, setBathroomCount] = useState(1);
   const [bedroomCount, setBedroomCount] = useState(1);
+  const [totalBedsCount, setTotalBedsCount] = useState(1);
   const [nightStayCount, setNightStayCount] = useState(1);
   const [cleaningFee, setCleaningFee] = useState(0);
   const [securityDeposit, setSecurityDeposit] = useState(0);
@@ -60,6 +61,10 @@ const RentalEvaluationForm = () => {
   }
   const handleOnclickBedroom = (newval) => {
     setBedroomCount(newval);
+    setTotalBedsCount(Math.min(newval, totalBedsCount));
+  }
+  const handleTotalBeds = (newval) => {
+    setTotalBedsCount(newval);
   }
   const handleOnclickNightStay = (newval) => {
     setNightStayCount(newval);
@@ -73,6 +78,28 @@ const RentalEvaluationForm = () => {
   const handleAddressChange = (e) => {
     setAddress(e.target.value);
   }
+  const getTestResponseData = () => {
+    return {
+      country: "Germany",
+      zip: '104',
+      street_addr: '1001 Berlin Drive',
+      long: 52.5218748,
+      lat: 13.3862362,
+      room_type: 1,
+      accomodates: 4,
+      bathrooms: 1.5,
+      bedrooms: 2,
+      beds: 2,
+      security_deposit: 0,
+      cleaning_fee: 50,
+      extra_people: 10,
+      cancellation_policy: 1,
+      price: 66.99,
+      minimum_nights: 1,
+      property_type: 1,
+      availability_365: 1
+  }
+}
   const getPostableData = () => {
     // country: STRING,
     // zip: STRING,
@@ -139,7 +166,7 @@ const RentalEvaluationForm = () => {
         </div>
         {/* minimum nights stay */}
         <div className={(stepCount == 2) ? 'nightStayCount wizardSection' : 'nightStayCount wizardSection hidden'}>
-            <div className='instructions'>Add the minimum number of nights required to rent your property...</div>          
+          <div className='instructions'>Add the minimum number of nights required to rent your property...</div>
           {[0, 1, 2, 3, 4, 5].map(elem => <div onClick={() => { handleOnclickNightStay(elem + 1) }} className={(nightStayCount > elem) ? 'nightsIcon icon selected' : 'nightsIcon icon'}>
             <MoonIcon />
           </div>)}
@@ -149,43 +176,51 @@ const RentalEvaluationForm = () => {
         </div>
         {/* number of bedrooms */}
         <div className={(stepCount == 3) ? 'bedroomCount wizardSection' : 'bedroomCount wizardSection hidden'}>
-            <div className='instructions'>Add the number of bedrooms your property has...</div>
+          <div className='instructions'>Add the number of bedrooms your property has...</div>
           <div onClick={() => { handleOnclickBedroom(0) }} className={(bedroomCount == 0) ? 'bedroomIcon selected icon' : 'bedroomIcon icon'}>
             <NoBedIcon />
           </div>
+            <input className="totalBeds"
+              type='number'
+              placeholder="Number of Beds"
+              value={totalBedsCount}
+              onChange={handleTotalBeds}>
+            </input>
           {[1, 2, 3, 4, 5].map(elem => <div onClick={() => { handleOnclickBedroom(elem) }} className={(bedroomCount > elem - 1) ? 'bedroomIcon icon selected' : 'bedroomIcon icon'}>
             <BedIcon />
           </div>)}
         </div>
         {/* type of property */}
         <div className={(stepCount == 4) ? 'propertyTypes wizardSection' : 'propertyTypes wizardSection hidden'}>
-            <div className='instructions'>Add the type of property you are renting (ie: guesthouse, apartment, condo, house, or other)</div>
-          {[1, 2, 3, 4, 5].map(elem => <div onClick={() => { handleOnclickPropertyType(elem) }} className={(propertyType == elem) ? 'propertyTypeIcon icon selected' : 'propertyTypeIcon icon'}>
-            {(elem == 1) ? <GuestHouse /> : ''}
-            {(elem == 2) ? <Apartment /> : ''}
-            {(elem == 3) ? <Condo /> : ''}
-            {(elem == 4) ? <House /> : ''}
-            {(elem == 5) ? <Other /> : ''}
-            }}
-          </div>)}
+          <div className='instructions'>Add the type of property you are renting (ie: guesthouse, apartment, condo, house, or other)</div>
+          {[1, 2, 3, 4, 5].map(elem => <div
+            onClick={() => { handleOnclickPropertyType(elem) }}
+            className={(propertyType == elem) ? 'propertyTypeIcon icon selected' : 'propertyTypeIcon icon'}>
+            {(elem === 1) ? <GuestHouse /> : ''}
+            {(elem === 2) ? <Apartment /> : ''}
+            {(elem === 3) ? <Condo /> : ''}
+            {(elem === 4) ? <House /> : ''}
+            {(elem === 5) ? <Other /> : ''}
+          </div>
+          )}
         </div>
         {/* number of bathrooms */}
         <div className={(stepCount == 5) ? 'bathroomCount wizardSection' : 'bathroomCount wizardSection hidden'}>
-            <div className='instructions'>Add the number of bathrooms your property has...</div>
+          <div className='instructions'>Add the number of bathrooms your property has...</div>
           {[0, 1, 2, 3, 4, 5].map(elem => <div onClick={() => { handleOnclickBathroom(elem + 1) }} className={(bathroomCount > elem) ? 'bathroomIcon icon selected' : 'bathroomIcon icon'}>
             <Bathrooms />
           </div>)}
         </div>
         {/* cancellation policy */}
         <div className={(stepCount == 6) ? 'cancellationPolicies wizardSection' : 'cancellationPolicies wizardSection hidden'}>
-            <div className='instructions'>What is your cancellation policy?  Select your cancellation policy below.</div>
+          <div className='instructions'>What is your cancellation policy?  Select your cancellation policy below.</div>
           {[[0, 'Flexible'], [1, '14'], [3, '30'], [4, '60']].map(elem => <div onClick={() => { setCancellationPolicy(elem[0]) }} className={(cancellationPolicy == elem[0]) ? 'cancellationPolicy selected' : 'cancellationPolicy'}>
             {elem[1]}
           </div>)}
         </div>
         {/* cleaning fee */}
         <div className={(stepCount == 7) ? 'cleaningFeePolicy wizardSection' : 'cleaningFeePolicy wizardSection hidden'}>
-            <div className='instructions'>Do you charge a fee for cleaning? Add how much your charge below...</div>
+          <div className='instructions'>Do you charge a fee for cleaning? Add how much your charge below...</div>
           <div className='cleaningFee'>
             <div className={(cleaningFee > 0) ? 'maidContainer icon selected' : 'maidContainer icon'}>
               <CleaningFee />
@@ -200,7 +235,7 @@ const RentalEvaluationForm = () => {
         </div>
         {/* security deposit */}
         <div className={(stepCount == 8) ? 'securityDepositContainer wizardSection' : 'securityDepositContainer wizardSection hidden'}>
-            <div className='instructions'>Does your property require a security deposit?  Add that number below...</div>
+          <div className='instructions'>Does your property require a security deposit?  Add that number below...</div>
           <div className='securityDepositIcon icon'>
             <SecurityDeposit />
           </div>
@@ -212,8 +247,8 @@ const RentalEvaluationForm = () => {
           </input>
         </div>
         <div className='wizardNav'>
-          <button disabled={(stepCount == 1) ? "disabled" : ""} onClick={() => { handleStepDecrement() }} >Previous</button>
-          <button disabled={(stepCount == 7) ? "disabled" : ""} onClick={() => { handleStepIncrement() }} >Next</button>
+          <button disabled={(stepCount === 1) ? "disabled" : ""} onClick={() => { handleStepDecrement() }} >Previous</button>
+          <button disabled={(stepCount === 7) ? "disabled" : ""} onClick={() => { handleStepIncrement() }} >Next</button>
         </div>
       </form>
     </div>
@@ -225,6 +260,8 @@ const RentalEvaluationForm = () => {
 
 export default RentalEvaluationForm;
 
+// username: value1
+// password: value2
 
 // id - int(20) - Your DB id for the listing. Will be used to GET data when no changes need to be made
 // summary - string < 300 - Brief description of the property and keywords ("Great Location!")
--- client/src/components/RentalPrediction.js
+++ client/src/components/RentalPrediction.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './RentalPrediction.css';
 
 const RentalPrediction = (props) => {
+    let encodedAddress = props.street_address || '43 Friedrich Strasse'
+    encodedAddress = encodeURI(encodedAddress);
     return <div> 
         {props.predictedPrice}
+        <iframe className='RentalPredictionMap' src="http://maps.google.com/maps?width=300&amp;height=300&amp;hl=en&amp;q=43%20Friedrich%20Strasse%2C%20Berlin%2C%20Germany+(My%20AirBnB%20Property)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"></iframe>
     </div>
 }
 
