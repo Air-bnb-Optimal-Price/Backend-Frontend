f45e8a740f260709445999843575fbcc763027d7
Gideon <reachgiddy@gmail.com>
2020-02-04 22:47:39 -0500

    Sending post request

--- client/src/components/RegisterLoginSection/RegisterContainer/Register.js
+++ client/src/components/RegisterLoginSection/RegisterContainer/Register.js
@@ -5,41 +5,89 @@ import { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { withFormik, Form, Field } from 'formik';
 
-function Register({values, errors, touched, status}) {
-const [register, setRegister] = useState();
+function Register() {
+const [register, setRegister] = useState({
+        username: '',
+        password: '',
+        password2: ''
+});
 
-    useEffect(() => {
-        Axios.post("/user/register")
-        .then(res => {
-            console.log(res);    
-        }).catch(error => {
-            console.log(error)
-        })
-    }, []);
+const handleSubmit = async e => {
+        e.preventDefault();
+        if(register.password !== register.password2) {
+            console.log("Passwords do not match!");
+        } else {
+            const newUser = {
+                username: e.target.value,
+                password: e.target.value,
+                password2: e.target.value
+            }
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            const body = JSON.stringify(newUser);
+            const res = await Axios.post('/user/register', body, config)
+                await res.then(res => {
+                    console.log(res);
+                })
+                console.log(res);
+            } catch(err) {
+                console.log("Error:", err);
+            }
+        } 
+    }  
+    
+const handleInput = e => {
+    setRegister({
+        ...register,
+        [e.target.name]: e.target.value
+    });
+}
 
-    function handleSubmit(){
-
-    }
     return (
-        <div className="register-container">
-            <Form className="form">
+        <div onSubmit={handleSubmit} className="register-container">
+            <form className="form">
                 <div className="input-wrapper">
                     <h2>Register</h2>
                     <div className="input-container">
                         <label htmlFor="user">Username</label>
-                        <Field id="user" type='text' name="username" placeholder="Enter Username"/>
+                        <input 
+                            id="user" 
+                            type='text' 
+                            onChange={handleInput}
+                            name="username" 
+                            value={register.username} 
+                            placeholder="Enter Username" 
+                            autoComplete="off"
+                        />
                     </div>
-                    <div className="input-container">
-                        <label htmlFor="email">Email</label>
-                        <Field id="email" type='email' name="email" placeholder="Enter Email"/> 
-                    </div>  
+
                     <div className="input-container">
                         <label htmlFor="password">Password</label>
-                        <Field id="password" type='password' name="password" placeholder="Enter Password"/> 
+                        <input 
+                            id="password" 
+                            type='password' 
+                            onChange={handleInput}
+                            name="password" 
+                            value={register.password} 
+                            placeholder="Enter Password"
+                            autoComplete="off"
+                        /> 
                     </div>
                     <div className="input-container">
-                        <label htmlFor="comfirm-password">Confirm Password</label>
-                        <Field id="comfirm_password" type='password' name="comfirmPassword" placeholder="Enter Password again"/> 
+                        <label htmlFor="passwordConfirm">Confirm Password</label>
+                        <input 
+                            id="passwordComfirm" 
+                            type='password' 
+                            onChange={handleInput}
+                            name="password2" 
+                            value={register.password2}
+                            placeholder="Enter Password again"
+                            autoComplete="off"
+                        /> 
                     </div>
                     
                 
@@ -48,15 +96,34 @@ const [register, setRegister] = useState();
                         <p>Already Signed Up?</p><Link to="/login">Login</Link>
                     </div>
                 </div>
-            </Form>
+            </form>
         </div>
     );
 }
 
-const FormikRegister = withFormik({
-    
+// const FormikRegister = withFormik({
+//     mapPropsToValues(props){
+//         return {
+//             username: props.user
+//         }
+//     }
 
 
-})(Register);
 
-export default FormikRegister;
+
+// })(Register);
+
+export default Register;
+
+
+//  useEffect(() => {
+//         Axios.post("/user/register", {
+
+
+//         })
+//         .then(res => {
+//             console.log(res);    
+//         }).catch(error => {
+//             console.log(error)
+//         })
+//     }, []);
