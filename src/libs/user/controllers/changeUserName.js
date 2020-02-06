1ea97e8d9eb6fa5e3c2123adc2ed60adac3f6cc7
igor <embiggenerd@gmail.com>
2020-02-06 11:39:07 -0500

    bug fix

--- src/libs/user/controllers/changeUserName.js
+++ src/libs/user/controllers/changeUserName.js
@@ -1,12 +1,17 @@
-import { changeUserName } from '../services'
+// import { changeUserName } from '../services'
 
 module.exports = async (req, res, next) => {
     const { body, userID } = req
 
     try {
-        
+        const user = await changeUserName(body, userID)
+        console.log('changedUser', user)
 
-    }catch(e){
+        res.json({
+            username: user.name
+        })
+
+    } catch (e) {
         next(e)
     }
 
