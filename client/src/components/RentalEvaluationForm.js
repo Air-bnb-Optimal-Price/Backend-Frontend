3503670fd1430535c8c30549af6c3f991d83b288
igor <embiggenerd@gmail.com>
2020-02-07 00:02:29 -0500

    mergesies

--- client/src/components/RentalEvaluationForm.js
+++ client/src/components/RentalEvaluationForm.js
@@ -273,20 +273,6 @@ const RentalEvaluationForm = () => {
             onChange={handleAddressChange}>
           </input>
         </div>
-<<<<<<< HEAD
-
-=======
-        <div className='wizardNav'>
-          <button className='formButton' disabled={(stepCount === 1) ? "disabled" : ""} onClick={() => { handleStepDecrement() }} >Previous</button>
-          <button className='formButton' disabled={(stepCount === 9) ? "disabled" : ""} onClick={() => { handleStepIncrement() }} >Next</button>
-          <button className='formButton' disabled={(stepCount === 10) ? "disabled" : ""} onClick={() => { handleStepIncrement() }} >Submit</button>
-          {/* <input className='submitButton'
-            type='submit' 
-            value='Submit'
-             onSubmit={handleSubmit} >
-            </input> */}
-        </div>
->>>>>>> 8ac4e11ee8dc6ac52a4147ea4dceab94ff649572
       </form>
       <div className='wizardNav'>
         <button disabled={(stepCount === 1) ? "disabled" : ""} onClick={() => { handleStepDecrement() }} >Previous</button>
